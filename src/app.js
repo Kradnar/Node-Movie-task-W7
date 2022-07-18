7c0fa7ce1ece159b966b5a84a3414bd6a7cbe603
Kradnar <pete.daniel.1975@gmail.com>
2022-07-18 12:36:48 +0100

    2nd commit

--- src/app.js
+++ src/app.js
@@ -6,12 +6,17 @@ const app = (argv) => {
       const newMovie = new Movie(argv[3], argv[4], argv[5]);
       newMovie.add()
       break;
-    case "and":
-      const newMovie2 = new Movie(argv[7], argv[8], argv[9])
-      newMovie2.add()
     default:
       console.log("Something went wrong")
     break;
+  }
+  switch (argv[6]) {
+    case "and":
+      const newMovie2 = new Movie(argv[7], argv[8], argv[9])
+      newMovie2.add()
+      break;
+    default:
+      console.log("No extra movies")
   }
 };
 
